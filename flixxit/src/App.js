904756e0c27092aa14f012e68ddd0e0845a60f12
admin <admin@admins-MacBook-Air.local>
2023-08-30 02:02:17 +0530

    pricing

--- flixxit/src/App.js
+++ flixxit/src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Pricing from "./components/Pricing/Pricing";
+import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        {/* <Route path="/about" component={AboutComponent} /> */}
-        {/* <Route path="/pricing" component={PricingComponent} /> */}
-        <Route path="/" component={Home} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Header />
+    //   <Routes>
+    //     {/* <Route path="/about" component={AboutComponent} /> */}
+    //     <Route path="/pricing" component={Pricing} />
+    //     <Route path="/" component={Home} />
+    //   </Routes>
+    // </Router>
+
+    <Pricing />
   );
 };
 
---
+++ flixxit/src/components/Pricing/Pricing.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Pricing.scss";
+import { FaCheck, FaTimes } from "react-icons/fa";
+
+const Pricing = () => {
+  const plans = [
+    {
+      name: "Basic",
+      price: "$9.99/month",
+      features: [
+        { name: "Feature 1", available: true },
+        { name: "Feature 2", available: true },
+        { name: "Feature 3", available: false },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+      ],
+    },
+    {
+      name: "Standard",
+      price: "$19.99/month",
+      features: [
+        { name: "Feature 1", available: true },
+        { name: "Feature 2", available: true },
+        { name: "Feature 3", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 3", available: false },
+        { name: "Feature 3", available: false },
+        { name: "Feature 3", available: false },
+      ],
+    },
+    {
+      name: "Premium",
+      price: "$29.99/month",
+      features: [
+        { name: "Feature 1", available: true },
+        { name: "Feature 2", available: true },
+        { name: "Feature 3", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+        { name: "Feature 4", available: true },
+      ],
+    },
+  ];
+
+  return (
+    <div className="pricing">
+      <h2 className="features-heading">Choose the plan that's right for you</h2>
+
+      <div className="cards">
+        {plans.map((plan, index) => (
+          <div className="card" key={index}>
+            <h2 className="card-name">{plan.name}</h2>
+            <p className="card-price">{plan.price}</p>
+            <ul className="features">
+              {plan.features.map((feature, index) => (
+                <li key={index}>
+                  {feature.available ? (
+                    <FaCheck className="feature-icon available" />
+                  ) : (
+                    <FaTimes className="feature-icon not-available" />
+                  )}
+                  {feature.name}
+                </li>
+              ))}
+            </ul>
+            <button className="choose-plan-button">Choose Plan</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
