40296e3966e5f9b37e968fb58b31e8104fb91b9a
admin <admin@admins-MacBook-Air.local>
2023-09-11 13:12:52 +0530

    login and sign up success

--- flixxit/src/App.js
+++ flixxit/src/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
+// import Home from "./components/Home/Home";
 import Pricing from "./components/Pricing/Pricing";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
@@ -14,26 +14,32 @@ import LoginHome from "./components/LoginHome/LoginHome";
 import { SignUp } from "./components/SignUp/SignUp";
 import Genere from "./components/LoginHome/Genere";
 import AboutMovie from "./components/LoginHome/AboutMovie";
+import Signup from "./components/SignUp/Sign";
+import Login from "./components/SignUp/Login";
+import Home from "./components/SignUp/Home";
 
 const App = () => {
   return (
     <Router>
       <div className="app-container">
         {/* <Header /> */}
-        {/* <div className="content">
+        <LoginHeader />
+        <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms-of-use" element={<TermsOfUse />} />
             <Route path="/signin" element={<SignUp />} />
-            <Route path="/about-movie/id" element={<AboutMovie />} />
+            <Route path="/about-movie/id" element={<AboutMovie />} /> */}
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
           </Routes>
-        </div> */}
-        <LoginHeader />
+        </div>
         {/* <LoginHome /> */}
-        <AboutMovie />
+        {/* <AboutMovie /> */}
         {/* <Genere /> */}
         <Footer />
       </div>
