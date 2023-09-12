3a197a8adaa9ae74339da7437483af1ad58ee041
admin <admin@admins-MacBook-Air.local>
2023-09-12 18:34:19 +0530

    movie info

--- flixxit/src/components/LoginHome/Genere/MovieDetails.jsx
+++ flixxit/src/components/LoginHome/Genere/MovieDetails.jsx
@@ -1,21 +1,72 @@
-// MovieDetails.js
 import React from "react";
 import "./MovieDetails.css";
 
-const MovieDetails = ({ movie }) => {
+const MovieDetails = ({ movie, reviews }) => {
+  console.log(movie, reviews);
+
+  const renderReviews = () => {
+    if (reviews.length === 0) {
+      return <p>No reviews available.</p>;
+    }
+
+    return (
+      <div className="reviews">
+        <h2>Reviews:</h2>
+        <ul>
+          {reviews.map((review, index) => (
+            <li key={index}>
+              <h3>
+                Author: &nbsp;
+                {review.author}
+              </h3>
+              <br />
+              {review.content}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   return (
-    <div className="movie-details">
-      <div className="movie-poster">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-        />
+    <div>
+      <div className="movie-details">
+        <div className="movie-poster">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+        </div>
+        <div className="movie-info">
+          <h1>{movie.title}</h1>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+
+          <p>
+            <strong>Genres:</strong>{" "}
+            {movie.genres.map((genre) => genre.name).join(", ")}
+          </p>
+          <p>
+            <strong>Runtime:</strong> {movie.runtime} minutes
+          </p>
+          <p>
+            <strong>User Score:</strong> {movie.vote_average}
+          </p>
+          <p>
+            <strong>Tagline:</strong> {movie.tagline}
+          </p>
+          <p>
+            <strong>Overview:</strong> {movie.overview}
+          </p>
+
+          <p>
+            <strong>Original Language:</strong> {movie.original_language}
+          </p>
+          {/* Add more details as needed */}
+        </div>
       </div>
-      <div className="movie-info">
-        <h1>{movie.title}</h1>
-        <p>{movie.description}</p>
-        {/* Add more details as needed */}
-      </div>
+      {renderReviews()}
     </div>
   );
 };
--- flixxit/src/components/LoginHome/Genere/MovieDetailsPage.jsx
+++ flixxit/src/components/LoginHome/Genere/MovieDetailsPage.jsx
@@ -1,29 +1,136 @@
-// MovieDetailsPage.js
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import axios from "axios";
+// import MovieDetails from "./MovieDetails";
+
+// const API_KEY = "634e2f77ea5af8af8758e53e75fe8937"; // Replace with your TMDb API key
+
+// const fetchMovieDetails = async (movieId) => {
+//   try {
+//     const response = await axios.get(
+//       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+//     );
+
+//     if (response.status === 200) {
+//       return response.data;
+//     }
+//   } catch (error) {
+//     console.error("Error fetching movie details:", error);
+//     return null;
+//   }
+// };
+
+// const MovieDetailsPage = () => {
+//   const { movieId } = useParams();
+//   const [movieDetails, setMovieDetails] = useState(null);
+
+//   useEffect(() => {
+//     const fetchDetails = async () => {
+//       try {
+//         const details = await fetchMovieDetails(movieId);
+//         setMovieDetails(details);
+//       } catch (error) {
+//         console.error("Error fetching movie details: ", error);
+//       }
+//     };
+
+//     fetchDetails();
+//   }, [movieId]);
+
+//   const fetchMovieReviews = async (movieId) => {
+//     try {
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+//       );
+
+//       if (response.status === 200) {
+//         return response.data.results;
+//       }
+//     } catch (error) {
+//       console.error("Error fetching movie reviews:", error);
+//       return [];
+//     }
+//   };
+
+//   return (
+//     <div className="movie-details-page">
+//       {movieDetails && <MovieDetails movie={movieDetails} />}
+//     </div>
+//   );
+// };
+
+// export default MovieDetailsPage;
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchMovieDetails } from "../../utils/tmdb"; // Import your function for fetching movie details
-import MovieDetails from "./MovieDetails"; // Import the MovieDetails component
+import axios from "axios";
+import MovieDetails from "./MovieDetails";
+
+const API_KEY = "634e2f77ea5af8af8758e53e75fe8937"; // Replace with your TMDb API key
+
+const fetchMovieReviews = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    if (response.status === 200) {
+      return response.data.results;
+    }
+  } catch (error) {
+    console.error("Error fetching movie reviews:", error);
+    return [];
+  }
+};
+
+const fetchMovieDetails = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    return null;
+  }
+};
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
+  const [movieReviews, setMovieReviews] = useState([]);
 
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        const details = await fetchMovieDetails(movieId); // Use your fetch function here
+        const details = await fetchMovieDetails(movieId);
         setMovieDetails(details);
       } catch (error) {
         console.error("Error fetching movie details: ", error);
       }
     };
 
+    const fetchReviews = async () => {
+      try {
+        const reviews = await fetchMovieReviews(movieId);
+        setMovieReviews(reviews);
+      } catch (error) {
+        console.error("Error fetching movie reviews: ", error);
+      }
+    };
+
     fetchDetails();
+    fetchReviews();
   }, [movieId]);
 
   return (
     <div className="movie-details-page">
-      {movieDetails && <MovieDetails movie={movieDetails} />}
+      {movieDetails && (
+        <MovieDetails movie={movieDetails} reviews={movieReviews} />
+      )}
     </div>
   );
 };
