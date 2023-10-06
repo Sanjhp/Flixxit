94738585ca3e2569d12dd91418e136a5969cdb0e
admin <admin@admins-MacBook-Air.local>
2023-10-06 10:51:53 +0530

    logout implemented

--- flixxit/src/components/LoginNavbar/LoginHeader.jsx
+++ flixxit/src/components/LoginNavbar/LoginHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { FaSearch, FaUser, FaBars } from "react-icons/fa";
+import { FaSearch, FaSignOutAlt } from "react-icons/fa";
 import styles from "./LoginHeader.module.css";
 import logo from "../../assets/logo.png";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,36 +7,13 @@ import { Link, useNavigate } from "react-router-dom";
 const LoginHeader = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
-  const [genres, setGenres] = useState([]);
   const navigate = useNavigate();
   const sidebarRef = useRef(null);
-
-  const toggleSidebar = (event) => {
-    event.stopPropagation(); // Prevent event propagation to the document
-    setSidebarOpen(!isSidebarOpen);
-  };
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/signin");
     window.location.reload();
-  };
-
-  useEffect(() => {
-    fetchTMDBGenres();
-  }, []);
-
-  const fetchTMDBGenres = async () => {
-    try {
-      const API_KEY = "634e2f77ea5af8af8758e53e75fe8937";
-      const response = await fetch(
-        `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
-      );
-      const data = await response.json();
-      setGenres(data.genres);
-    } catch (error) {
-      console.error("Error fetching genres:", error);
-    }
   };
 
   useEffect(() => {
@@ -63,15 +40,6 @@ const LoginHeader = () => {
   return (
     <div className={styles["app-container"]}>
       <header className={styles["login-header"]}>
-        {/* <div className={styles["left"]}>
-          <button
-            id="toggle-button"
-            className={styles["toggle-button"]}
-            onClick={toggleSidebar}
-          >
-            <FaBars />
-          </button>
-        </div> */}
         <div className={styles["left"]}>
           <Link to="/home">
             <img src={logo} alt="Logo" className={styles["logo"]} />
@@ -90,35 +58,12 @@ const LoginHeader = () => {
               <FaSearch className={styles["search-icon"]} />
             </Link>
           </div>
-          <FaUser className={styles["user-icon"]} />
-        </div>
-      </header>
-      {isSidebarOpen && (
-        <div className={styles["modal-container"]} ref={sidebarRef}>
-          <div className={styles["modal-sidebar"]}>
-            <div className={styles["tabs"]}>
-              {genres.map((genre) => (
-                <Link
-                  to={`/genre-search?query=${genre.id}`}
-                  key={genre.id}
-                  className={styles["genre-tab"]}
-                >
-                  {genre.name}
-                </Link>
-              ))}
-              <Link to="/settings" className={styles["settings-logout-tab"]}>
-                Settings
-              </Link>
-              <span
-                className={styles["settings-logout-tab"]}
-                onClick={handleLogout}
-              >
-                Logout
-              </span>
-            </div>
+          <div className={styles["logout-container"]} onClick={handleLogout}>
+            <FaSignOutAlt className={styles["logout-icon"]} />
+            <span className={styles["logout-text"]}>Logout</span>
           </div>
         </div>
-      )}
+      </header>
     </div>
   );
 };
